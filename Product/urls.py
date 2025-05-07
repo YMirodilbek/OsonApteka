391ca37963b2666a02534edc0f1321c6b92db593
oyatullohm <marziyahoni@gmail.com>
2025-05-07 06:49:08 +0500

    f

--- Product/urls.py
+++ Product/urls.py
@@ -5,8 +5,8 @@ from .views import *
 
 urlpatterns = [
     path('',Index),
-    path("add_to_cart/<int:product_id>/", add_to_cart, name="add_to_cart"),
     path('cart/',cart_view, name="cart"),
+    path("add_to_cart/<int:product_id>/", add_to_cart, name="add_to_cart"),
     path("increase-quantity/<int:item_id>/", increase_quantity, name="increase_quantity"),
     path("decrease-quantity/<int:item_id>/", decrease_quantity, name="decrease_quantity"),
     path('remove_from_cart/<int:item_id>/', DeleteProduct, name="delete"),
