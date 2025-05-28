766b8055dc8cbd360f5e27deff924f242212262b
oyatullohm <marziyahoni@gmail.com>
2025-05-28 22:00:54 +0500

    f

--- Admin/settings.py
+++ Admin/settings.py
@@ -84,12 +84,11 @@ CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', 'http://localhost']
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+from .set_database import LOCAL_DATABASE
+
+DATABASES = LOCAL_DATABASE
+
+
 
 
 # Password validation
