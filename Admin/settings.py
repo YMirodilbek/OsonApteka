d7453a257d204804c57a7545bbfeca3c114a2d8b
oyatullohm <marziyahoni@gmail.com>
2025-05-29 20:05:32 +0500

    f

--- Admin/settings.py
+++ Admin/settings.py
@@ -22,13 +22,10 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env.str('SECRET_KEY')
 
 
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ["*"]
-
-
-
 
 INSTALLED_APPS = [
     # 'jet',
