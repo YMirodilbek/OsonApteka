c886a0c95ed920035b9278e1a8e701dff4fc3975
oyatullohm <marzoyahoni@gmail.com>
2025-05-30 03:51:56 +0000

    f

--- Admin/urls.py
+++ Admin/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path('',include('Product.urls')),
     path('front/',include('tmp.urls'))
 ]
-# if settings.DEBUG:
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 handler404=handler_404
